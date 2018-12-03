db0da8c203354dd6c2e4f765ef46490ed74b2cc8
Nick George <nicholas.george32@gmail.com>
2018-12-02 20:13:55 -0700

    trying to speed up emacs init

--- init.el
+++ init.el
@@ -10,35 +10,49 @@
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
-
+(setq package-enable-at-startup nil)
+(setq package-archives  '(("melpa" . "http://melpa.org/packages/")
+                          ("gnu" . "http://elpa.gnu.org/packages/")
+                          ("org" . "http://orgmode.org/elpa/")))
 ;; set up package
-(require 'package)
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/")t)
-;; from https://emacs.stackexchange.com/questions/8182/how-to-use-org-plus-contrib
-;; below recommended from http://orgmode.org/elpa.html
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(setq package-load-list '(all))
-;; verify org installed
-(unless (package-installed-p 'org)
-  (package-install 'org))
-;; initialize packages
-(package-initialize)
+(eval-when-compile
+  (require 'package)
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; (add-to-list 'package-archives
+;;              '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/")t)
+;; ;; from https://emacs.stackexchange.com/questions/8182/how-to-use-org-plus-contrib
+;; ;; below recommended from http://orgmode.org/elpa.html
+;; (package-initialize)
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
+;; (setq package-load-list '(all))
+;; ;; verify org installed
+;; (unless (package-installed-p 'org)
+;;   (package-install 'org))
+;; ;; initialize packages
+;; (package-initialize)
 
 ;;Bootstrap use-package
 ;; Install use-package if its not already there
 ;; use package to configure the rest of the packages. 
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
 
 ;;From use-package README
-(eval-when-compile
-  (require 'use-package))
+;; (eval-when-compile
+;;   (require 'use-package))
 (require 'diminish)                ;; if you use :diminish
 (require 'bind-key)                ;; if you use any :bind variant
 (server-start)
